f5450306f39aa4e283b44eb15dfc86d199c47a62
vczanini <42414244+vczanini@users.noreply.github.com>
2018-09-11 22:53:10 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,36 @@
+//vinicius de carvalho zanini
+//11811EBI013
+
+
+#include <stdio.h>
+
+int main ()
+{
+	int ax = 0, x = 0;
+	char bit [256];
+	printf ("Digite o numero binario para ser  verificado: \n");
+	gets (bit);
+	for(bit[x];bit[x]!= '\0';x++)
+	{
+		if (ax == 0 && bit [x] == '0')
+			ax = 0;
+		else if (ax == 0 && bit [x] == '1')
+			ax = 1;
+		else if (ax == 1 && bit [x] == '1')
+			ax = 0;
+		else if (ax == 1 && bit [x] == '0')
+			ax = 2;
+		else if (ax == 2 && bit [x] == '0')
+			ax = 1;
+		else if (ax == 2 && bit [x] == '1')
+			ax = 2;
+	}
+
+	printf ("Sequencia: %s \n", bit);
+	if (ax == 0)
+		printf ("Eh multiplo de 3");
+	else
+		printf ("Nao eh");
+
+	return 0;
+}
