baa9327371de7548c4385ff027cd21e05b9d5462
vczanini <42414244+vczanini@users.noreply.github.com>
2018-09-25 22:16:46 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,37 +1,33 @@
-/*P1.c*/
+/*P1*/
 /*vinicius de carvalho zanini*/
 /*11811EBI013*/
-
-
-#include <stdio.h>
-
-int main ()
+#include<stdio.h>
+int main()
 {
-	int ax = 0, x = 0;
-	char bit [256];
-	printf ("Digite o numero binario para ser  verificado: \n");
-	gets (bit);
-	for(bit[x];bit[x]!= '\0';x++)
+	int seq=0, x=0;
+	char bit[256];
+	printf("digite o numero verificado: ");
+	scanf("%s", bit);
+	while(bit[x] != '\0')
 	{
-		if (ax == 0 && bit [x] == '0')
-			ax = 0;
-		else if (ax == 0 && bit [x] == '1')
-			ax = 1;
-		else if (ax == 1 && bit [x] == '1')
-			ax = 0;
-		else if (ax == 1 && bit [x] == '0')
-			ax = 2;
-		else if (ax == 2 && bit [x] == '0')
-			ax = 1;
-		else if (ax == 2 && bit [x] == '1')
-			ax = 2;
+		if(seq== 0 && bit[x] == '0')
+		seq = 0;
+		else if(seq ==0 && bit[x] == '1')
+		seq = 1;
+		else if(seq == 1 && bit[x] == '1')
+		seq = 0;
+		else if(seq == 1 && bit[x] =='0')
+		seq = 2;
+		else if(seq == 2 && bit[x] == '0')
+		seq = 1;
+		else if(seq == 2 && bit[x] == '1')
+		seq = 2;
+		x++;
 	}
-
-	printf ("Sequencia: %s \n", bit);
-	if (ax == 0)
-		printf ("Eh multiplo de 3");
+	printf("temos como sequencia %s \n ", bit);
+	if(seq==0)
+	printf("multiplo de 3.");
 	else
-		printf ("Nao eh");
-
+	printf("Nao multiplo de 3.");
 	return 0;
 }
