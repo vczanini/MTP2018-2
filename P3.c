a9bb8406b935232870084f765d15913ac90820bb
vczanini <42414244+vczanini@users.noreply.github.com>
2018-10-15 22:29:12 -0300

    Add files via upload

---
+++ P3.c
@@ -0,0 +1,23 @@
+/*P3.C */
+/* Vinicius de Carvalho Zanini */
+/* 11811EBI013 */
+
+#include <stdio.h>
+int main()
+{
+	char numeroini[276], numerofi[276];
+	int numero, a = 0, b = 0;
+    printf("digite o numero: ");
+	scanf("%s", numeroini);
+	for (a=0; numeroini[a] != '\0'; a++)
+	{
+		if(numeroini[a]>= 48 && numeroini[a]<= 57)
+		{
+			numerofi[b] = numeroini[a];
+			b++;
+		}
+	}
+	numero = atoi(numerofi);
+	printf("%d", numero);
+	return 0;
+}
